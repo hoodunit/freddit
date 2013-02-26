02d26de5801ae52dc3143899da70c383b55f5e01
Nicholas Kariniemi <nicholas.kariniemi@gmail.com>
2013-02-26 22:44:09 +0200

    Added PostCtrl to controllers, wrapped it in a module to work with tests

--- app/js/controllers/controllers.js
+++ app/js/controllers/controllers.js
@@ -2,11 +2,12 @@
  * Base controller. New controllers need to be registered here.
  **/ 
 
-define(['angular', 'js/controllers/subreddits'], 
-       function (angular, SubredditsCtrl) {
+define(['angular', 'js/controllers/subreddits', 'js/controllers/post'], 
+       function (angular, SubredditsCtrl, PostCtrl) {
  	 var controllers = angular.module('controllers', ['services']);
 
  	 controllers.controller('SubredditsCtrl', SubredditsCtrl);
+ 	 controllers.controller('PostCtrl', PostCtrl);
 
  	 return controllers;
 });
--- app/js/controllers/post.js
+++ app/js/controllers/post.js
@@ -1,9 +1,15 @@
-function PostCtrl($http, $scope, $routeParams, $location) {
-  $scope.infoPost = [];
-  $http.jsonp('http://www.reddit.com/by_id/t3_'+ $routeParams.id + '.json?jsonp=JSON_CALLBACK').
-    success(function(object){
-      $scope.infoPost = object.data.children[0].data;
-    });
-}
+define(function () {
+  'use strict';
 
-PostCtrl.$inject = ['$http','$scope', '$routeParams', '$location'];
+  function PostCtrl($http, $scope, $routeParams, $location) {
+    $scope.infoPost = [];
+    $http.jsonp('http://www.reddit.com/by_id/t3_'+ $routeParams.id + '.json?jsonp=JSON_CALLBACK').
+      success(function(object){
+        $scope.infoPost = object.data.children[0].data;
+      });
+  }
+
+  PostCtrl.$inject = ['$http','$scope', '$routeParams', '$location'];
+
+  return PostCtrl;
+});

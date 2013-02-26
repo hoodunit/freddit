a772c0eee8ba323d3d8834ce0f8d7d8913cce0ae
Joona Heinikoski <joonamo@gmail.com>
2013-02-26 21:38:53 +0200

    Nicer intendation in subreddits.js

--- app/js/controllers/subreddits.js
+++ app/js/controllers/subreddits.js
@@ -2,25 +2,25 @@ define(function () {
   'use strict';
 
   function SubredditsCtrl($scope, $routeParams, $location, $http) {
-  $scope.posts = [];
-  $http.jsonp('http://reddit.com/r/lolcats.json?jsonp=JSON_CALLBACK').
-    success(function(data){
-      for (var i in data.data.children)
-      {
-        // quick hack to show pics linking to imgur url instead of the picture
-        var post = data.data.children[i];
-        var url = post.data.url;
-        if (url.indexOf("imgur") >= 0 && url.indexOf("http://i.") < 0)
+    $scope.posts = [];
+    $http.jsonp('http://reddit.com/r/lolcats.json?jsonp=JSON_CALLBACK').
+      success(function(data){
+        for (var i in data.data.children)
         {
-            // everything is .jpg, right?
-            data.data.children[i].data.url = url + ".jpg";
+          // quick hack to show pics linking to imgur url instead of the picture
+          var post = data.data.children[i];
+          var url = post.data.url;
+          if (url.indexOf("imgur") >= 0 && url.indexOf("http://i.") < 0)
+          {
+              // everything is .jpg, right?
+              data.data.children[i].data.url = url + ".jpg";
+          }
         }
-      }
-      $scope.posts = data.data.children;
-    });
-}
+        $scope.posts = data.data.children;
+      });
+  }
 
-SubredditsCtrl.$inject = ['$scope', '$routeParams', '$location', '$http'];
+  SubredditsCtrl.$inject = ['$scope', '$routeParams', '$location', '$http'];
 
   return SubredditsCtrl;
 });

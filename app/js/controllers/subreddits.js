9333a433c113992c7f1eb88a3f64be2dc5f5299c
Joona Heinikoski <joonamo@gmail.com>
2013-02-26 21:36:51 +0200

    Moved tile_view.css from app/shared/style to app/css. Re-applied the imgur hack.

--- app/js/controllers/subreddits.js
+++ app/js/controllers/subreddits.js
@@ -2,14 +2,25 @@ define(function () {
   'use strict';
 
   function SubredditsCtrl($scope, $routeParams, $location, $http) {
-    $scope.posts = [];
-    $http.jsonp('http://reddit.com/r/lolcats.json?jsonp=JSON_CALLBACK').
-      success(function(data){
-        $scope.posts = data.data.children;
-      });
-  }
+  $scope.posts = [];
+  $http.jsonp('http://reddit.com/r/lolcats.json?jsonp=JSON_CALLBACK').
+    success(function(data){
+      for (var i in data.data.children)
+      {
+        // quick hack to show pics linking to imgur url instead of the picture
+        var post = data.data.children[i];
+        var url = post.data.url;
+        if (url.indexOf("imgur") >= 0 && url.indexOf("http://i.") < 0)
+        {
+            // everything is .jpg, right?
+            data.data.children[i].data.url = url + ".jpg";
+        }
+      }
+      $scope.posts = data.data.children;
+    });
+}
 
-  SubredditsCtrl.$inject = ['$scope', '$routeParams', '$location', '$http'];
+SubredditsCtrl.$inject = ['$scope', '$routeParams', '$location', '$http'];
 
   return SubredditsCtrl;
 });

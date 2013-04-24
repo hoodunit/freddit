59ad8ce18a825f65ce4b46d8b6663dc27c3e8bdc
Joona <joona@miniKiivi.(none)>
2013-04-24 13:56:03 +0300

    First test of settings panel

--- app/js/controllers/controllers.js
+++ app/js/controllers/controllers.js
@@ -2,13 +2,14 @@
  * Base controller. New controllers need to be registered here.
  **/ 
 
-define(['angular','js/controllers/overview', 'js/controllers/subreddits', 'js/controllers/post'], 
-       function (angular,OverviewCtrl, SubredditsCtrl, PostCtrl) {
+define(['angular','js/controllers/overview', 'js/controllers/subreddits', 'js/controllers/post', 'js/controllers/settings'], 
+       function (angular,OverviewCtrl, SubredditsCtrl, PostCtrl, SettingsCtrl) {
  	 var controllers = angular.module('controllers', ['services']);
 
  	 controllers.controller('SubredditsCtrl', SubredditsCtrl);
  	 controllers.controller('PostCtrl', PostCtrl);
  	 controllers.controller('OverviewCtrl',OverviewCtrl);
+ 	 controllers.controller('SettingsCtrl',SettingsCtrl);
 
  	 return controllers;
 });

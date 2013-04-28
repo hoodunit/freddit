f640bcb2a8ab6ccd65161bb68242b2b752c5ac8b
Tuikku Anttila <tuikku.anttila@gmail.com>
2013-04-28 13:32:13 +0300

    deleted some comments

--- app/js/controllers/subreddits.js
+++ app/js/controllers/subreddits.js
@@ -7,7 +7,7 @@ define(function () {
     $scope.orderBy = $routeParams.order;
 
     if($scope.orderBy){
-    	console.log("got parameters");
+    	
     	$scope.posts = RedditAPI.getSubredditPostsSortedBy($scope.subredditName,$scope.orderBy);
     	if($scope.posts === null){
     		$scope.posts = RedditAPI.getSubredditPosts($scope.subredditName);
@@ -15,9 +15,7 @@ define(function () {
     } else {
     	$scope.posts = RedditAPI.getSubredditPosts($scope.subredditName);
     }
-    
-    //$scope.posts = RedditAPI.getSubredditPosts($scope.subredditName);
-    
+
   }
 
   SubredditsCtrl.$inject = ['$scope', '$routeParams', 'RedditAPI'];

7061c07f0b031dd4cb836db3d52fa4c155e846c0
Joona <joona@miniKiivi.(none)>
2013-04-28 16:47:20 +0300

    Improved settings page template. Prevented some error messages from appearing for no reason.

--- app/js/controllers/settings.js
+++ app/js/controllers/settings.js
@@ -2,7 +2,17 @@ define(function () {
   'use strict';
 
   function SettingsCtrl($scope, RedditAPI) {
+    $scope.nsfw = true;
 
+    $scope.addSubreddit = function(name) {
+      alert("Should now be adding " + name);
+    }
+
+    $scope.removeSubreddit = function(name) {
+      alert("Should now be removing " + name);
+    }
+
+    $scope.subreddits = RedditAPI.getSubreddits();
   }
     
   SettingsCtrl.$inject = ['$scope', 'RedditAPI'];

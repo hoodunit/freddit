3ae0d5cd5875b42b3d7cb47b20a6cec5e6d3560b
Joona <joona@miniKiivi.(none)>
2013-05-05 12:14:34 +0300

    settings actually open from the left.

--- app/js/controllers/overview.js
+++ app/js/controllers/overview.js
@@ -1,7 +1,7 @@
 define(function () {
   'use strict';
 
-  function OverviewCtrl($scope, RedditAPI) {
+  function OverviewCtrl($scope, RedditAPI, Settings) {
     $scope.username = RedditAPI.getUsername();
     $scope.loggedIn = RedditAPI.loggedIn;
     
@@ -18,9 +18,45 @@ define(function () {
     };
 
     $scope.loadSubreddits();
+
+    // For settings drawer
+    $scope.showSettingsDrawer = false;
+    $scope.toggleSettingsDrawer = function () {
+      $scope.showSettingsDrawer = !$scope.showSettingsDrawer;
+    }
+
+    $scope.nsfwFlag = Settings.getNSFWFlag();
+
+    $scope.checkNSFW = function(){
+      Settings.setNSFWFlag($scope.nsfwFlag);
+    };
+
+    $scope.addSubreddit = function(name) {
+      alert("Should now be adding " + name);
+    }
+
+    $scope.removeSubreddit = function(name) {
+      alert("Should now be removing " + name);
+    }
+
+    $scope.install = function(name) {
+      var manifest_url = "http:0.0.0.0:8081/manifest.webapp";
+      var myapp = navigator.mozApps.install(manifest_url);
+      myapp.onsuccess = function(data) {
+      // App is installed, remove button
+        
+        //$scope.installed = true;
+      };
+      myapp.onerror = function() {
+      // App wasn't installed, info is in
+      // installapp.error.name
+     };
+    }
+
+    $scope.subreddits = RedditAPI.getSubreddits();
   }
 
-  OverviewCtrl.$inject = ['$scope', 'RedditAPI'];
+  OverviewCtrl.$inject = ['$scope', 'RedditAPI', 'Settings'];
 
   return OverviewCtrl;
 
